b5b4c2295915b9be6507c6fa677a97b29f6ea957
isaeva2av <118861738+isaeva2av@users.noreply.github.com>
2022-12-11 20:08:35 +0300

    Update task_5.py

--- Лабораторная работа №5/task_5.py
+++ Лабораторная работа №5/task_5.py
@@ -1,10 +1,10 @@
-import random
+from random import sample
 import string
 
 
 def get_random_password(length) -> str:
     # Генирируем значения с помощью первой константы (буквами верхнего, нижнего регистра), и второй - цифры.
-    random_password = string.ascii_letters + string.digits
+    random_password = string.ascii_uppercase + string.ascii_lowercase + string.digits
     random_string = ''.join(random.sample(random_password, length))  # Формируем случайную строку определенной длины.
     return random_string
 
